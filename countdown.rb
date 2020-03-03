049ab5f7aa65eb0adec25b7608fd3ebec486ca4e
Pharia Le <pharia.le@gmail.com>
2020-03-03 23:55:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
+def countdown(number)
+  counter = number
+  
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -=1
+  end
+  
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+
+def countdown_with_sleep(number)
+  
 end
