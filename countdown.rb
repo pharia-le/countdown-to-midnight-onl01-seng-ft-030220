1cd1e4768d5b877990eafa60aa469155b0355e61
Pharia Le <pharia.le@gmail.com>
2020-03-04 00:07:24 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
-#write your code here
 
-def countdown
+
+def countdown(number)
+  counter = number
+  
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -=1
+  end
+  
+  "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(number)
+  counter = number
+  
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    sleep(1)
+    counter -=1
+  end
+  
+  "HAPPY NEW YEAR!"
 end
